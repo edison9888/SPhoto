c37d823abb0c9e4f7eb52ce794eeeb01c990dabb
SunJiangting <sun@Sunstar.local>
2012-08-03 21:33:39 +0800

    Initial Commit

---
+++ SPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPhoto
+//
+//  Created by SunJiangting on 12-8-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ SPhoto/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  SPhoto
+//
+//  Created by SunJiangting on 12-8-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
